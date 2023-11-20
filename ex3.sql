940b04fd2a01e31d4bfafdcff9248db03705688d
Júlia Rafaela <124639320+Julia-Rafaela@users.noreply.github.com>
2023-11-20 18:01:25 -0300

    Add files via upload

---
+++ ex3.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE ex3
+GO
+USE ex3
+
+GO
+CREATE TABLE paciente(
+cpf           VARCHAR(11)  NOT NULL,
+nome          VARCHAR(30)  NOT NULL,
+rua           VARCHAR(30)  NOT NULL,
+num           INT          NOT NULL,
+bairro        VARCHAR(30)  NOT NULL,
+telefone      CHAR(08)     NOT NULL,
+data_nasc     DATE         NOT NULL,
+PRIMARY KEY (cpf)
+)
+
+GO
+CREATE TABLE medico(
+cod              INT          NOT NULL,
+nome             VARCHAR(30)  NOT NULL,
+especialidade    VARCHAR(30)  NOT NULL
+PRIMARY KEY(cod)
+)
+
+GO 
+CREATE TABLE prontuario(
+data_prontuario        DATE         NOT NULL,
+cpf_paciente           VARCHAR(11)  NOT NULL,
+cod_med                INT          NOT NULL,
+diagnostico            VARCHAR(50)  NOT NULL,
+medicamento            VARCHAR(50)  NOT NULL
+PRIMARY KEY(data_prontuario, cpf_paciente, cod_med)
+FOREIGN KEY(cpf_paciente) REFERENCES paciente(cpf),
+FOREIGN KEY(cod_med) REFERENCES medico(cod)
+)
+
+DROP TABLE paciente
+
+INSERT INTO paciente VALUES
+('35454562890', 'José Rubens', 'Campos Salles', 2750, 'Centro', '21450998', '1954-10-18'),
+('29865439810', 'Ana Claudia', 'Sete de Setembro', 178, 'Centro', '97382764', '1960-05-29'),
+('82176534800', 'Marcos Aurélio', 'Timóteo Penteado', 236, 'Vila Galvão', '68172651', '1980-09-24'),
+('12386758770', 'Maria Rita', 'Castello Branco', 7765, 'Vila Rosália', ' ', '1975-03-30'),
+('92173458910', 'Joana de Souza', 'XV de Novembro', 298, 'Centro', '21276578', '1944-04-24')
+
+INSERT INTO medico VALUES
+(1, 'Wilson Cesar', 'Pediatra'),
+(2, 'Marcia Matos', 'Geriatra'),
+(3, 'Carolina Oliveira', 'Ortopedista'),
+(4, 'Vinicius Araujo', 'Clínico Geral')
+
+INSERT INTO prontuario VALUES
+('2020-09-10', '35454562890', 2, 'Reumatismo', 'Celebra'),
+('2020-09-10', '92173458910', 2, 'Renite Alérgica', 'Allegra'),
+('2020-09-12', '29865439810', 1, 'Inflamação de garganta', 'Nimesulida'),
+('2020-09-13', '35454562890', 2, 'H1N1', 'Tamiflu'),
+('2020-09-15', '82176534800', 4, 'Gripe', 'Resprin'),
+('2020-09-15', '12386758770', 3, 'Braço Quebrado', 'Dorflex + Gesso')
+
+--Consultar:				
+--Nome e Endereço (concatenado) dos pacientes com mais de 50 anos				
+SELECT nome,
+       rua +' '+ CAST(num AS VARCHAR) + ' '+ bairro AS endereço
+FROM paciente
+WHERE DATEDIFF(YEAR, data_nasc, GETDATE()) > 50
+
+--Qual a especialidade de Carolina Oliveira	
+SELECT especialidade
+FROM medico
+WHERE nome = 'Carolina Oliveira'
+			
+--Qual medicamento receitado para reumatismo				
+SELECT medicamento
+FROM   prontuario
+WHERE diagnostico = 'reumatismo'

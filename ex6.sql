85e1cd4a5b2e5119fba04621044ec2b2205f3829
Júlia Rafaela <124639320+Julia-Rafaela@users.noreply.github.com>
2023-12-05 10:19:33 -0300

    Update ex6.sql
    with subquery

--- ex6.sql
+++ ex6.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE viagem
 GO
 USE viagem
@@ -13,11 +12,11 @@ PRIMARY KEY (Codigo)
 )
 
 INSERT INTO Motorista VALUES
-(12341,	'Julio Cesar',		'1978-04-18',	'São Paulo'),
+(12341,	'Julio Cesar',		'1978-04-18',	'SÃ£o Paulo'),
 (12342,	'Mario Carmo',		'2002-07-29',	'Americana'),
 (12343,	'Lucio Castro',		'1969-12-01',	'Campinas'),
-(12344,	'André Figueiredo',	'1999-05-14',	'São Paulo'),
-(12345,	'Luiz Carlos',		'2001-01-09',	'São Paulo')
+(12344,	'AndrÃ© Figueiredo',	'1999-05-14',	'SÃ£o Paulo'),
+(12345,	'Luiz Carlos',		'2001-01-09',	'SÃ£o Paulo')
 
 GO
 CREATE TABLE Onibus(			
@@ -30,10 +29,10 @@ PRIMARY KEY (Placa)
 
 
 INSERT INTO	Onibus VALUES
-('adf0965',   	'Mercedes', 1999,	'Leito'),               
-('bhg7654',  	'Mercedes', 2002,	'Sem Banheiro'),        
-('dtr2093',   	'Mercedes',	2001,	'Ar Condicionado'),     
-('gui7625',  	'Volvo', 2001,	'Ar Condicionado')    
+('adf0965', Â Â 	'Mercedes', 1999,	'Leito'), Â Â Â Â Â Â Â Â Â Â Â Â Â Â 
+('bhg7654',Â Â 	'Mercedes',Â 2002,	'Sem Banheiro'), Â Â Â Â Â Â Â 
+('dtr2093', Â Â 	'Mercedes',	2001,	'Ar Condicionado'), Â Â Â Â 
+('gui7625',Â Â 	'Volvo',Â 2001,	'Ar Condicionado')Â Â Â Â 
 
 GO
 CREATE TABLE Viagem(					
@@ -55,32 +54,55 @@ INSERT INTO Viagem VALUES
 (103,	'bhg7654', 12345,	'14:00:00',	'22:00:00',	'Rio de Janeiro'),
 (104,	'dtr2093', 12344,	'18:00:00',	'21:00:00',	'Sorocaba')
 
--- Consultar, da tabela viagem, todas as horas de chegada e saída, convertidas em formato HH:mm (108) e seus destinos
-SELECT CONVERT(VARCHAR(5), Hora_Chegada, 108) AS Hora_Chegada,
-    CONVERT(VARCHAR(5), Hora_Saida, 108) AS Hora_Saida,
-    Destino
-FROM Viagem			
+Select * from Viagem
+Select * from Onibus
+Select * from Motorista
+
+-- Consultar, da tabela viagem, todas as horas de chegada e saÃ­da, convertidas em formato HH:mm (108) e seus destinos
+SELECT CONVERT( VARCHAR(5), Hora_Chegada, 108) AS  Hora_Chegada,
+       CONVERT( VARCHAR(5), Hora_Saida, 108) AS Hora_Saida,
+	   Destino
+FROM Viagem
+		
 -- Consultar, com subquery, o nome do motorista que viaja para Sorocaba	
-SELECT m.Nome
+SELECT Nome
+FROM Motorista
+WHERE Nome IN(
+SELECT DISTINCT m.Nome
 FROM Motorista m, Viagem v
 WHERE m.Codigo = v.Motorista
-AND v.Destino = 'Sorocaba'
--- Consultar, com subquery, a descrição do ônibus que vai para o Rio de Janeiro		
-SELECT o.Descricao
+AND v.Destino = 'Sorocaba' )
+
+-- Consultar, com subquery, a descriÃ§Ã£o do Ã´nibus que vai para o Rio de Janeiro		
+SELECT Descricao
+FROM Onibus
+WHERE Descricao IN(
+SELECT DISTINCT o.Descricao
 FROM Onibus o, Viagem v
 WHERE o.Placa = v.Onibus
-AND v.Destino = 'Rio de Janeiro'					
--- Consultar, com Subquery, a descrição, a marca e o ano do ônibus dirigido por Luiz Carlos		
-SELECT o.Descricao,
-       o.Marca,
-	   o.Ano
-FROM Onibus o, Motorista M, Viagem v
+AND v.Destino = 'Rio de Janeiro')
+
+					
+-- Consultar, com Subquery, a descriÃ§Ã£o, a marca e o ano do Ã´nibus dirigido por Luiz Carlos		
+SELECT          o.Marca,
+				o.Placa
+FROM Onibus o, Viagem v, Motorista m
 WHERE o.Placa = v.Onibus
-AND v.Motorista = m.Codigo
-AND m.Nome =  'Luiz Carlos'						
--- Consultar o nome, a idade e a naturalidade dos motoristas com mais de 30 anos								
-SELECT nome,
-       DATEDIFF(YEAR, Data_nascimento, GETDATE()) AS  idade,
-	   naturalidade
+AND v.Motorista	= m.Codigo
+AND m.Nome IN(
+SELECT DISTINCT m.Nome
+FROM Onibus o, Viagem v, Motorista m
+WHERE o.Placa = v.Onibus
+AND v.Motorista	= m.Codigo	
+AND m.Nome = 'Luiz Carlos' )
+
+-- Consultar o nome, a idade e a naturalidade dos motoristas com mais de 30 anos			
+SELECT Nome,
+       DATEDIFF(YEAR, Data_nascimento, GETDATE()) AS Idade
+FROM Motorista	
+WHERE  DATEDIFF(YEAR, Data_nascimento, GETDATE()) IN (
+SELECT DISTINCT  DATEDIFF(YEAR, Data_nascimento, GETDATE()) AS Idade
 FROM Motorista
-WHERE  DATEDIFF(YEAR, Data_nascimento, GETDATE()) > 30
+WHERE  DATEDIFF(YEAR, Data_nascimento, GETDATE()) > 30 )
+
+
